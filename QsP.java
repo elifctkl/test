05e0f36ab5cf8055eaa921f598afc474a2fea239
elifctkl <72555165+elifctkl@users.noreply.github.com>
2021-11-10 15:41:10 +0100

    Create QsP.java

---
+++ QsP.java
@@ -0,0 +1,66 @@
+
+public class QsP {
+	
+	public static int anzDurchlauf = 0;
+	public static int anzVergleich = 0;
+	
+	public static void main(String args[]) {
+		int[] a = { 99, 0, 33, 27, 87, 80, 65, 77, 39, 88 };
+		System.out.print("Zu sortierendes Array:" + " ");
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("");
+		sortiere(a, 0, a.length - 1);
+		System.out.print("Sortiertes Array:" + "\n");
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		safe();
+	
+	}
+	
+	static void sortiere(int []array, int begin, int end) {
+		anzDurchlauf++;
+		if(begin<end) {
+			int pivotIndex = 1;
+			int pivot = array[end];
+			int begintemp = begin;
+			int endtemp = end;
+			
+			while(true) {
+				anzVergleich++;
+				while(array[begintemp]> pivot) {
+					begintemp++;
+				}
+				while(array[endtemp]< pivot) {
+					endtemp--;
+				}
+				
+				if(begintemp < endtemp) {
+					int hilf = array[begintemp];
+					array[begintemp] = array[endtemp];
+					array[endtemp] = hilf;
+					/*System.out.println("Begintemp: " + begintemp);
+					System.out.println("Endtemp: " + endtemp);
+					System.out.println("Pivot-Index: " + pivotIndex);*/
+				} else {
+					pivotIndex = endtemp;
+					//System.out.println("Pivot-Index: " + pivotIndex);
+					break;
+				}
+			}
+			
+			sortiere(array, begin, pivotIndex-1);
+			sortiere(array, pivotIndex +1, end);
+		}
+	}
+	
+	public static void safe(){
+		System.out.println("\nAnzahl Durchläufe: " + anzDurchlauf);
+		System.out.println("Anzahl Vergleiche: " + anzVergleich + "\n");
+	}
+
+}
