191d1f5f64b0e8fa625e93716a94acb4ab04f683
elifctkl <72555165+elifctkl@users.noreply.github.com>
2021-11-10 14:44:58 +0100

    Update and rename b.java to B.java

--- B.java
+++ B.java
@@ -1,6 +1,7 @@
+
 import javax.swing.JOptionPane;
 
-public class b {
+public class B {
     public static void main(String[] args) {
         int[] array = { 99,90,79,75,70,60,55,37,23,17,16,9,8,6,5,1};
         
@@ -19,20 +20,24 @@ public class b {
     }
 
     static int binarySearch(int[] array, int searchvalue, int bottom, int top) {
-        int mid = bottom + ((top - bottom) / 2);
+        
+        if (bottom <= top) {
 
-        System.out.println("\nbottom: " + bottom);
-        System.out.println("mid: " + mid);
-        System.out.println("top: " + top);
-        if (array[mid] == searchvalue) {
+            int mid = bottom + ((top - bottom) / 2);
+            
+            System.out.println("\nbottom: " + bottom);
+            System.out.println("mid: " + mid);
+            System.out.println("top: " + top);
+            
+            if (array[mid] == searchvalue) 
             return mid;
+            
+            if (array[mid] < searchvalue)
+                return binarySearch(array, searchvalue, bottom, mid-1);
+            else
+                return binarySearch(array, searchvalue, mid+1, top);
         }
-        if (array[mid] < searchvalue)
-            return binarySearch(array, searchvalue, bottom, mid-1);
-        else if (array[mid] > searchvalue)
-            return binarySearch(array, searchvalue, mid-1, top);
-        else
-            return -1;
+        return -1;
     
     }
 }
